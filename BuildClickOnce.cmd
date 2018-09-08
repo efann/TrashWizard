5ca4926fff31c23c09c3a2286030e87f0dff4a0a
Eddie Fann <efann@beowurks.com>
2018-09-07 22:03:44 -0500

    MSBuild Command Line fixed

--- BuildClickOnce.cmd
+++ BuildClickOnce.cmd
@@ -1,8 +1,7 @@
 @echo on
-set lcYear=%date:~10,4%
 set lcMSBuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
 rmdir bin /S /Q
 %lcMSBuild% /target:build
-%lcMSBuild% /target:publish /property:ApplicationVersion=%lcYear%.0.0.34
+%lcMSBuild% TrashWizard.csproj @TrashWizard.Prop.rsp
 
