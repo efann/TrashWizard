3a6a9a439de5386030c001a8026c0afe1407bb17
Eddie Fann <efann@beowurks.com>
2019-05-10 17:02:19 -0500

    Updating copyright

--- Win32/Windows32.cs
+++ Win32/Windows32.cs
@@ -2,7 +2,7 @@
 // Trash Wizard : a Windows utility program for maintaining your temporary files.
 //  =============================================================================
 //  
-// (C) Copyright 2007-2018, by Beowurks.
+// (C) Copyright 2007-2019, by Beowurks.
 //  
 // This application is an open-source project; you can redistribute it and/or modify it under 
 // the terms of the Eclipse Public License 2.0 (https://www.eclipse.org/legal/epl-2.0/). 
